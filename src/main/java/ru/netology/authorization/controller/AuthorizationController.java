ffdaa178d7f6db712e88edc5da4118fcea6f7a37
Dmitriy Petrov <nevermind1979@mail.ru>
2022-11-29 21:44:16 +0300

    add project

--- src/main/java/ru/netology/authorization/controller/AuthorizationController.java
+++ src/main/java/ru/netology/authorization/controller/AuthorizationController.java
@@ -1,8 +1,10 @@
 package ru.netology.authorization.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.netology.authorization.exception.InvalidCredentials;
+import ru.netology.authorization.exception.UnauthorizedUser;
 import ru.netology.authorization.model.Authorities;
 import ru.netology.authorization.service.AuthorizationService;
 
@@ -10,10 +12,25 @@ import java.util.List;
 
 @RestController
 public class AuthorizationController {
+    final
     AuthorizationService service;
+
+    public AuthorizationController(AuthorizationService service) {
+        this.service = service;
+    }
 
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
         return service.getAuthorities(user, password);
+    }
+
+    @ExceptionHandler(InvalidCredentials.class)
+    public ResponseEntity<String> handleInvalidCredentials (InvalidCredentials e) {
+        return new ResponseEntity<>( "Exception: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UnauthorizedUser.class)
+    public ResponseEntity<String> handleUnauthorizedUser (UnauthorizedUser e) {
+        return new ResponseEntity<>("Exception: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
--- src/main/java/ru/netology/authorization/service/AuthorizationService.java
+++ src/main/java/ru/netology/authorization/service/AuthorizationService.java
@@ -9,9 +9,14 @@ import ru.netology.authorization.model.Authorities;
 import java.util.List;
 @Service
 public class AuthorizationService {
+    final
     UserRepository userRepository;
 
-   public List<Authorities> getAuthorities(String user, String password) {
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<Authorities> getAuthorities(String user, String password) {
         if (isEmpty(user) || isEmpty(password)) {
             throw new InvalidCredentials("User name or password is empty");
         }
